c124154b39180eff654b84faaba0fe4c30e90f01
imbibek <shresthabibek5400@gmail.com>
2023-01-05 18:39:25 +0545

    Intro to 'var'

---
+++ datatypes.dart
@@ -0,0 +1,16 @@
+void main() {
+  int i = 10; //Numbers as int
+  bool isValid = true; //Boolean
+  String name = "Bibek"; //Strings
+  print(i);
+  print(isValid);
+  print(name);
+
+  //Instead we can use 'var' without having to specify the datatypes
+  var j = 20;
+  var fav_game = "Fifa 23";
+  var result = true;
+  print(j);
+  print(result);
+  print(fav_game);
+}
